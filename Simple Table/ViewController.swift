7fc5ff173efa47698362a00114700fdd4a4a2737
Burenkovuser <nb2207@yandex.ru>
2019-08-14 09:31:03 +0700

    Initial Commit

---
+++ Simple Table/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Simple Table
+//
+//  Created by Vasilii on 14/08/2019.
+//  Copyright © 2019 Vasilii Burenkov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
